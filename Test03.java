3d878d61f48d2f31688fa8729ecfab592df8055a
HyejuJin <77781111+HyejuJin@users.noreply.github.com>
2021-01-21 19:57:43 +0900

    Add files via upload

---
+++ Test03.java
@@ -0,0 +1,26 @@
+package test;
+import java.util.Scanner;
+
+public class Test03 {
+	
+	public static long fac(int n) {
+		if(n==1) {
+			System.out.println("1!=1");
+			return 1;
+		} else {
+			System.out.println(n + "!=" + n + "*" + (n-1) + "!");
+			return fac(n-1)*n;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("숫자를 입력하세요.");
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fac(n));
+		
+
+	
+	}
+}
