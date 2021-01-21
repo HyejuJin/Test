55e0e7a29b56321482b049bf92885fca3761381f
HyejuJin <77781111+HyejuJin@users.noreply.github.com>
2021-01-21 20:00:33 +0900

    Add files via upload

---
+++ Test02.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.Scanner;
+
+public class Test02 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("문장을 입력하세요.");
+		
+	}
+}
