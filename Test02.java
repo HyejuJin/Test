843ca1801acefa6a695c2efbc7e4d84f2b919eb4
HyejuJin <77781111+HyejuJin@users.noreply.github.com>
2021-01-21 20:03:53 +0900

    Add files via upload

--- Test02.java
+++ Test02.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Test02 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("¹®ÀåÀ» ÀÔ·ÂÇÏ¼¼¿ä.");
+		System.out.println("ë¬¸ì¥ì„ ì…ë ¥í•˜ì„¸ìš”.");
 		
 	}
 }
